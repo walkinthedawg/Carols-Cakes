e5e3f2924e6106faf36c6e0386e98cab07a029b3
Ubuntu <ubuntu@ip-172-31-92-99.ec2.internal>
2019-09-14 05:21:48 +0000

    trying to get Heroku to work still

--- carolscakes/settings.py
+++ carolscakes/settings.py
@@ -96,7 +96,9 @@ WSGI_APPLICATION = 'carolscakes.wsgi.application'
 #    }
 #}
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
